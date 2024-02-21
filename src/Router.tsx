ef281c15508c09824e0027de3c1cfb237f08fe74
Baloou <127010557+Marco-Jan@users.noreply.github.com>
2024-02-21 09:19:01 +0100

    add delete

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,6 +4,7 @@ import ErrorPage from './Components/ErrorElement';
 import Movie, {loader as movieLoader} from './Components/Movie'
 import {action as editAction} from './Components/EditMovie';
 import EditMovie from './Components/EditMovie';
+import { action as deleteAction } from './Components/Destroy';
 
 
 export default function Router() {
@@ -26,6 +27,12 @@ export default function Router() {
                     loader: movieLoader as any,
                     action: editAction as any
                 },
+                {
+                    path: "/movies/:id/destroy",
+                    action: deleteAction as any,
+                    
+                }
+                
             ]
         },
         
